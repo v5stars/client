e5ad6c449733549a006c01b4d9437c2a2312065a
shynome <shynome@gmail.com>
2019-07-10 07:11:18 +0800

    less path

--- lib/common/router_register.dart
+++ lib/common/router_register.dart
@@ -1,5 +1,5 @@
 import 'package:fluro/fluro.dart' show Router;
-import 'package:v5stars/pages/home/home.router.dart' as home;
+import 'package:v5stars/pages/home/router.dart' as home;
 
 void defineRoutes(Router router) {
   router.define("/", handler: home.routeHandler);
